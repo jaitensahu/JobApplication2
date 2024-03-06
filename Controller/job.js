0736ed1bc27cea1564ac94a147b92bfab500d69c
Jaiten <Jaitensahu9@gmail.com>
2024-03-06 21:09:04 +0530

    delete api added

--- Controller/job.js
+++ Controller/job.js
@@ -44,18 +44,33 @@ const editJob = async (req, res) => {
     );
     res.status(200).json({
       success: "true",
-      message: "Got all the jobs",
+      message: "Job Edited Successfully",
       data: allJobs,
     });
   } catch (error) {
     res.status(404).json({
       success: false,
-      message: "Unable to get jobs",
+      message: "Unable to edit jobs",
+      error:error,
     });
   }
 };
-const deleteJob = (req, res) => {
+const deleteJob = async (req, res) => {
   console.log(req, res);
+  try {
+   let result = await Jobs.deleteOne({
+      _id: {
+        $eq: req.body._id,
+      },
+   });
+    console.log(result);
+    res.status(200).json({
+      success: true,
+      message:"Deleted Successefully"
+    })
+  } catch (error) {
+    console.log(error);
+  }
 }
 const jobController = {
   createJob,
